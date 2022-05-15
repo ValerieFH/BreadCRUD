800fb454601f188581ec7b8163ab91a3c5217800
Valerie Haupt <valre@Valeries-MacBook-Pro-2.local>
2022-05-15 11:12:20 -0500

    added further bread routes

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const Bread = require('../models/bread')
 
 router.get('/', (req, res) => {
-    res.send("Breads controller woohoo")
+    res.send(Bread)
+})
+
+router.get('/:arrayIndex', (req, res) => {
+    const {  arrayIndex  } = req.params
+    res.send(Bread[arrayIndex])
 })
 
 module.exports = router
